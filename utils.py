14b97c1267d17e3d96a9be02c52b4817706a35b3
LiamHecht <liamhecht800@gmail.com>
2023-11-25 21:59:06 +0200

    added comments

--- utils.py
+++ utils.py
@@ -35,7 +35,6 @@ def get_ip(addr):
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        # doesn't even have to be reachable
         s.connect(('10.255.255.255', 1))
         local_ip = s.getsockname()[0]
     except Exception:
